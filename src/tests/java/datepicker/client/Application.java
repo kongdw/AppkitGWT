d44f1007d5eaf45c597774a51f6173a320fe6205
Christopher Bruno <cbruno@MacBoxPro-3.local>
2015-05-27 02:57:48 -0500

    fixes

--- src/tests/java/datepicker/client/Application.java
+++ src/tests/java/datepicker/client/Application.java
@@ -1,10 +1,13 @@
 package datepicker.client;
 
 
+import com.appkit.ui.client.widgets.input.date.CalendarUtil;
 import com.appkit.ui.client.widgets.input.date.DatePicker;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.ui.RootPanel;
+
+import java.util.Date;
 
 public class Application implements EntryPoint {
 
@@ -12,6 +15,12 @@ public class Application implements EntryPoint {
     public void onModuleLoad() {
 
         DatePicker datePicker = new DatePicker();
+
+        Date minDate = new Date();
+        CalendarUtil.addDaysToDate(minDate, -60);
+
+        datePicker.setMinimumDate(minDate);
+
 
         datePicker.setSize("150px", "28px");
         datePicker.getElement().getStyle().setMargin(30.0, Style.Unit.PX);
